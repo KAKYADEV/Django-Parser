f6af8c7d053b833b49fecdb876e937f3d101dd2e
KAKYADEV <man.senka@mail.ru>
2026-01-16 20:17:36 +0300

    Исправлена модель ReqSite и сделана новая миграция

--- parser/parse/models.py
+++ parser/parse/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class ReqSite(models.Model):
     name = models.CharField(max_length=31)
     url = models.URLField()
-    time_request = models.TimeField(auto_now_add=True)
-    time_response = models.TimeField()
-    duration = models.DurationField()
-    active = models.BooleanField()
+    time_request = models.DateTimeField(auto_now_add=True)
+    time_response = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField(null=True, blank=True)
+    active = models.BooleanField(default=True)
