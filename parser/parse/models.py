499ad4d6a60d27f1b26b3284e1793760826889c9
KAKYADEV <man.senka@mail.ru>
2026-01-16 19:48:25 +0300

    Добавлена первая модель ReqSite и выполнена первая миграция

--- parser/parse/models.py
+++ parser/parse/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ReqSite(models.Model):
+    name = models.CharField(max_length=31)
+    url = models.URLField()
+    time_request = models.TimeField(auto_now_add=True)
+    time_response = models.TimeField()
+    duration = models.DurationField()
+    active = models.BooleanField()
