cd83e2f97a84db15305d700d828ea3951aefe2c3
KAKYADEV <man.senka@mail.ru>
2026-01-23 20:33:38 +0300

    Добавлен путь ./sites/<id> к странице с деталями

--- parser/parse/urls.py
+++ parser/parse/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index),
-    path('sites/', req_site_list),
+    path('', index, name='index'),
+    path('sites/', req_site_list, name='req_site_list'),
+    path('sites/<int:pk>/', req_site_detail, name='req_site_detail'),
 ]
