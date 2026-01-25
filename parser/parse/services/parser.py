d1e8ffc401372e404b4841dd598a55bab866ef4f
KAKYADEV <man.senka@mail.ru>
2026-01-25 14:18:05 +0300

    Реализована функция run в классе Parser

--- parser/parse/services/parser.py
+++ parser/parse/services/parser.py
@@ -1,1 +1,24 @@
-# Здесь будет парсер
+from parse.models import ReqSite
+
+
+class Parser:
+    def __init__(self, site_id):
+        self.site_id = site_id
+
+    def run(self):
+        site = ReqSite.objects.get(pk=self.site_id)
+
+        try:
+            print(f"Запущен парсинг сайта: {site.url}")
+            # Здесь будет логика парсинга сайта
+
+            site.status = ReqSite.Site.DONE
+        except Exception as e:
+            site.status = ReqSite.Site.ERROR
+            print(f"Ошибка при парсинге сайта {site.url}: {e}")
+            raise e
+        finally:
+            site.save(update_fields=['status'])
+            return print("Парсер работает")
+
+        
