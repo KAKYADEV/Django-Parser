f52bfed366a3481b0785b37085096af6683472e7
KAKYADEV <man.senka@mail.ru>
2026-01-25 14:14:52 +0300

    Добавлен маршрут для start_parse

--- parser/parse/urls.py
+++ parser/parse/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('sites/', req_site_list, name='req_site_list'),
     path('sites/<int:pk>/', req_site_detail, name='req_site_detail'),
+    path('sites/<int:pk>/start_parse/', start_parse, name='start_parse'),
 ]
