df2fcd4eac35848d2ab9a593107d9c07c3e5abfe
KAKYADEV <man.senka@mail.ru>
2026-01-25 14:12:09 +0300

    Добавлено поле status и класс Site для текстового описания состояний

--- parser/parse/models.py
+++ parser/parse/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 class ReqSite(models.Model):
+
+    class Site(models.TextChoices):
+        NEW = 'new', 'Новый'
+        PROCESSING = 'processing', 'В процессе'
+        DONE = 'done', 'Завершен'
+        ERROR = 'error', 'Ошибка'
+        
+
+
     name = models.CharField(max_length=31)
     url = models.URLField()
+
+    status = models.CharField(
+        max_length=15,
+        default=Site.NEW,
+        choices=Site.choices,
+    )
+
     time_request = models.DateTimeField(auto_now_add=True)
-    time_response = models.DateTimeField(auto_now_add=True)
+    time_response = models.DateTimeField(null=True, blank=True)
     duration = models.DurationField(null=True, blank=True)
     active = models.BooleanField(default=True)
 
