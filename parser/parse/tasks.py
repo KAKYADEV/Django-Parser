3ce9de4f2f9f3100eba0735afaa8b7c4b1dff9e7
KAKYADEV <man.senka@mail.ru>
2026-01-31 19:06:29 +0300

    Созданы @shared_task для асинхронного вызова парсера

---
+++ parser/parse/tasks.py
@@ -0,0 +1,9 @@
+from celery import shared_task
+from parse.services.parser import Parser
+
+
+@shared_task
+def start_background_parse(pk):
+    print(f"Задача парсинга запущена для сайта с ID: {pk}")
+    parser = Parser(pk)
+    parser.run()
