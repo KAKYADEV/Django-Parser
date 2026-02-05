90805be34f3d8d04760d79d3f400ec04643f4ede
KAKYADEV <man.senka@mail.ru>
2026-02-05 21:26:31 +0300

    Добавлен backend в redis

--- parser/parser/settings.py
+++ parser/parser/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
