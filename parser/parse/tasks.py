431ee5d020556f34126120d279f4a60c136df7dc
KAKYADEV <man.senka@mail.ru>
2026-02-07 20:46:29 +0300

    Перенесены смена значения поля status и добавление результата в модель ParsedData

--- parser/parse/tasks.py
+++ parser/parse/tasks.py
@@ -1,10 +1,27 @@
 from celery import shared_task
 from parse.services.parser import Parser
+from parse.models import *
 
 
 @shared_task
 def start_background_parse(pk):
     print(f"Задача парсинга запущена для сайта с ID: {pk}")
+
+    site = ReqSite.objects.get(pk=pk)
+    site.status = ReqSite.Site.PROCESSING
+    site.save(update_fields=['status'])
+
     parser = Parser(pk)
-    parser.run()
+    result = parser.run()
+
+    ParsedData.objects.create(
+        site = site,
+        title = result['title'],
+        description = result['description'],
+        keywords = result['keywords'],
+    )
+
+    site.status = ReqSite.Site.DONE
+    site.save(update_fields=['status'])
+
     return 'success'
