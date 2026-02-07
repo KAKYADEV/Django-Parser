7746ae96c08751cc7df83bcc775274253fc46cee
KAKYADEV <man.senka@mail.ru>
2026-02-07 21:15:27 +0300

    Подстроен код под получение ссылки, а не id (модели теперь полностью вынесены из файла)

--- parser/parse/services/parser.py
+++ parser/parse/services/parser.py
@@ -3,26 +3,25 @@ import time
 
 
 class Parser:
-    def __init__(self, site_id):
-        self.site_id = site_id
+    def __init__(self, site_url):
+        self.site_url = site_url
 
     def run(self):
-        site = ReqSite.objects.get(pk=self.site_id)
 
         try:            
-            print(f"Запущен парсинг сайта: {site.url}")  
+            print(f"Запущен парсинг сайта: {self.site_url}")  
             time.sleep(15)  # Здесь будет логика парсинга сайта
            
         except Exception as e:
-            site.status = ReqSite.Site.ERROR
-            print(f"Ошибка при парсинге сайта {site.url}: {e}")
+            #site.status = ReqSite.Site.ERROR
+            print(f"Ошибка при парсинге сайта {self.site_url}: {e}")
             raise e
         
         finally:
             return {
-                'title': f"Заголовок для {site.name}",
-                'description': f"Описание для {site.name}",
-                'keywords': f"ключевые слова для {site.name}",
+                'title': f"{self.site_url}",
+                'description': f"Описание для {self.site_url}",
+                'keywords': f"Ключевые слова для {self.site_url}",
             }
 
         
