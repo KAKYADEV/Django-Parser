b93235d98b19aea4dd1c075647f59ad0b3e6ed65
KAKYADEV <man.senka@mail.ru>
2026-02-17 20:49:42 +0300

    Добавлены функции get_description и get_keywords

--- parser/parse/services/scrap.py
+++ parser/parse/services/scrap.py
@@ -17,9 +17,40 @@ def get_title(soup):
     try:
         scrap_logger.info(f"Запущен поиск тэга title")
         title = soup.find('title')
-        title = title.text
+        if title:
+            title = title.text
+        else:
+            title = 'No title found'
     except Exception as e:
         scrap_logger.exception(f"{e}", exc_info=True)
         raise TagNameNotExist from e
     else:
         return title
+    
+def get_description(soup):
+    try:
+        scrap_logger.info(f"Запущен поиск description")
+        description = soup.find('meta', attrs={'name': 'description'})
+        if description:
+            description = description.get('content', 'No description found')
+        else:
+            description = 'No description found'
+    except Exception as e:
+        scrap_logger.exception(f"{e}", exc_info=True)
+        raise TagNameNotExist from e
+    else:
+        return description
+    
+def get_keywords(soup):
+    try:
+        scrap_logger.info(f"Запущен поиск keywords")
+        keywords = soup.find('meta', attrs={'name': 'keywords'})
+        if keywords:
+            keywords = keywords.get('content', 'No keywords found')
+        else:
+            keywords = 'No keywords found'
+    except Exception as e:
+        scrap_logger.exception(f"{e}", exc_info=True)
+        raise TagNameNotExist from e
+    else:
+        return keywords
