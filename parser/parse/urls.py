4328dfaba77981aa0549df402d26ea7948b4d9d6
KAKYADEV <man.senka@mail.ru>
2026-02-19 20:57:46 +0300

    Изменен маршрут с добавлением класса-представления ReqSiteListView.as_view()

--- parser/parse/urls.py
+++ parser/parse/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('sites/', req_site_list, name='req_site_list'),
+    path('sites/', ReqSiteListView.as_view(), name='req_site_list'),
     path('sites/<int:pk>/', req_site_detail, name='req_site_detail'),
     path('sites/<int:pk>/start_parse/', start_parse, name='start_parse'),
 ]
