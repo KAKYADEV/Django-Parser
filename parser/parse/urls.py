c661ba5177ed19040fee6e5e947dae7f27f93b41
KAKYADEV <man.senka@mail.ru>
2026-02-20 21:20:32 +0300

    Добавлен маршрут к странице с результатом

--- parser/parse/urls.py
+++ parser/parse/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('sites/', ReqSiteListView.as_view(), name='req_site_list'),
     path('sites/<int:pk>/', req_site_detail, name='req_site_detail'),
     path('sites/<int:pk>/start_parse/', start_parse, name='start_parse'),
+    path('sites/<int:pk>/results/', ParsedDataDetailView.as_view(), name='parsed_data_detail'),
 ]
