ac70f817ed8f36b27fbc8b824563a33b6152c0e3
KAKYADEV <man.senka@mail.ru>
2026-02-21 21:23:32 +0300

    Chenged the route for the main page

--- parser/parser/urls.py
+++ parser/parser/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
  
 urlpatterns = [
     path('admin/', dj_admin.site.urls),
-    path('parse/', include('parse.urls')),
+    path('', include('parse.urls')),
 ]
