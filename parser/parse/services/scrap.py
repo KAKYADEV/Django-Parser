685b524326d7f939ef6b9e066824fbc526926b59
KAKYADEV <man.senka@mail.ru>
2026-03-26 19:43:01 +0300

    Added functions to get headers and images

--- parser/parse/services/scrap.py
+++ parser/parse/services/scrap.py
@@ -54,3 +54,44 @@ def get_keywords(soup):
         raise TagNameNotExist from e
     else:
         return keywords
+    
+# Add oportunity to get headers, ... (need to change model ParsedData, add a multidata field, make migrations)
+
+def get_header(soup):
+    try:
+        header_list = []
+        scrap_logger.info(f"Запущен поиск тэга h1")
+        headers = soup.find_all('h1')
+        for header in headers:
+            if header:
+                header = header.text
+                header_list.append(header)
+            else:
+                header = 'No header found'
+                header_list.append(header)
+    except Exception as e:
+        scrap_logger.exception(f"{e}", exc_info=True)
+        raise TagNameNotExist from e
+    else:
+        return header_list
+    
+def get_images(soup):
+    try:
+        image_list = []
+        scrap_logger.info(f"Запущен поиск тэга img")
+        images = soup.find_all('img')
+        for image in images:
+            if image:
+                image_dict = {
+                    'src': image.get('src', 'No image found'),
+                    'alt': image.get('alt', 'No alt text found')
+                }
+                image_list.append(image_dict)
+            else:
+                image = 'No image found'
+                image_list.append(image)
+    except Exception as e:
+        scrap_logger.exception(f"{e}", exc_info=True)
+        raise TagNameNotExist from e
+    else:
+        return image_list
