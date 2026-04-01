9d02e8d95c40f58c11a5b670223f2e87af6d1070
KAKYADEV <man.senka@mail.ru>
2026-04-01 22:14:20 +0300

    Add 'about' page route

--- parser/parse/urls.py
+++ parser/parse/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
+    path('about/', MainPageView.as_view(), name='about'),
 ]
