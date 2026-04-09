1724635c5d781885098b2df911e14f9a91aab5f7
KAKYADEV <man.senka@mail.ru>
2026-04-09 21:59:13 +0300

    Enhance parser error handling and improve WebDriver options for better performance in headless mode

--- parser/parse/services/parser.py
+++ parser/parse/services/parser.py
@@ -4,6 +4,11 @@ from selenium import webdriver
 from .scrap import *
 from .exceptions import *
 import logging
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException
+from selenium.common.exceptions import TimeoutException
 
 
 parser_logger = logging.getLogger(__name__)
@@ -26,8 +31,14 @@ class Parser:
             options = webdriver.ChromeOptions()
             options.add_experimental_option('excludeSwitches', ['enable-logging'])
             options.add_argument("--headless=new")
+            options.add_argument("--window-size=1920,1080")
+            options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36")
             driver = webdriver.Chrome(options=options)
+
             driver.get(self.site_url)
+            wait = WebDriverWait(driver, timeout=20, poll_frequency=1, ignored_exceptions=[NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException])
+            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h1, h2, h3')))
+
             html_source = driver.page_source
             soup = BeautifulSoup(html_source, "html.parser")
 
@@ -37,7 +48,13 @@ class Parser:
             headers = get_header(soup)
             images = get_images(soup)
 
-        except ParserError as e:
+        except TimeoutException:
+            parser_logger.exception("TimeoutException", exc_info=True)
+            with open('exc.html', 'a', encoding='utf-8') as f:
+                f.write(driver.page_source)
+            raise ParserError("An unexpected error occurred during parsing")
+
+        except ParserError:
             parser_logger.exception("ParserError", exc_info=True)
             raise
         else:
