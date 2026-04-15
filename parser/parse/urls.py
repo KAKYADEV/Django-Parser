c264edcbee486a2928d2e381b2609b39013ba0ad
KAKYADEV <man.senka@mail.ru>
2026-04-15 17:18:40 +0300

    Remove unused route for site detail view in URLs

--- parser/parse/urls.py
+++ parser/parse/urls.py
@@ -5,7 +5,6 @@ from .views import *
 urlpatterns = [
     path('', MainPageView.as_view(), name='index'),
     path('sites/', ReqSiteListView.as_view(), name='req_site_list'),
-    path('sites/<int:pk>/', req_site_detail, name='req_site_detail'),
     path('sites/<int:pk>/start_parse/', start_parse, name='start_parse'),
     path('sites/<int:pk>/results/', ParsedDataDetailView.as_view(), name='parsed_data_detail'),
     path('register/', RegisterUserView.as_view(), name='register'),
